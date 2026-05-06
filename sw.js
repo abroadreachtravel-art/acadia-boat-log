aab658d08ffe211656f695beac147270825bcb41
abroadreachtravel-art <abroadreachtravel@gmail.com>
2026-05-06 19:51:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'acadia-v1';
+const ASSETS = [
+  '/acadia-boat-log/',
+  '/acadia-boat-log/index.html',
+  '/acadia-boat-log/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first for Google APIs, cache first for app shell
+  if (e.request.url.includes('googleapis.com') || e.request.url.includes('accounts.google.com')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    }))
+  );
+});
